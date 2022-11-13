9b5db71d9f9df57c1ab0d5b5a7fec9422bec5fe4
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-13 17:57:33 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Tweetinvi;
+
+var userClient = new TwitterClient(args[0], args[1], args[2], args[3]);
+
+var user = await userClient.Users.GetAuthenticatedUserAsync();
+
+var tweet = await userClient.Tweets.PublishTweetAsync("Teste Bot");
