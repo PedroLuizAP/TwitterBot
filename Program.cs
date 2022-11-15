57ed218983eb8cce2cbf1625600df6f97ded70c2
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-15 13:26:37 -0300

    add TwitterHelper

--- Program.cs
+++ Program.cs
@@ -1,23 +1,31 @@
 ﻿using Tweetinvi;
 using Tweetinvi.Parameters;
+using TwitterBot.Helper;
+
+//var userHelper = new TwitterHelper(args[0], args[1], args[2], args[3]);
 
 var userClient = new TwitterClient(args[0], args[1], args[2], args[3]);
 
 var user = await userClient.Users.GetAuthenticatedUserAsync();
 
-//string[] searchTerms = new[]
-//   {
-//        "@sseraphini",        
-//        "#paneladev",
-//        "#bolhadev"
-//    };
+string[] searchTerms = new[]
+   {
+        "@sseraphini",
+        "#paneladev",
+        "#bolhadev"
+    };
 
-//SearchTweetsParameters parameters = new(string.Join(" OR ", searchTerms)) 
-//{
-//     //Since = DateTime.UtcNow
-//};
+var dateSince = DateTime.UtcNow.AddMinutes(-5);
 
-var searchResponse = await userClient.Search.SearchTweetsAsync("pedroluizap");
+SearchTweetsParameters parameters = new(string.Join(" OR ", searchTerms))
+{
+    Since = dateSince,
+    SearchType = Tweetinvi.Models.SearchResultType.Recent,
+    Filters = Tweetinvi.Parameters.Enum.TweetSearchFilters.Safe,
+    Locale= "br",   
+};
+
+var searchResponse = await userClient.Search.SearchTweetsAsync(parameters);
 
 var tweets = searchResponse.ToList();
 
