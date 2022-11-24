b7d83c5d8e4d4793a365d03c96d2bde55fd8c511
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-24 00:24:12 -0300

    fix SearchTweets

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using Tweetinvi;
+using Tweetinvi.Core.Events;
+using Tweetinvi.Core.Extensions;
+using Tweetinvi.Core.Models;
 using Tweetinvi.Parameters;
 using TwitterBot.Helper;
 
@@ -15,18 +18,36 @@ string[] searchTerms = new[]
         "#bolhadev"
     };
 
-var dateSince = DateTime.UtcNow.AddMinutes(-5);
+var dateSince = DateTime.Now;
+//(bolhadev OR paneladev OR sseraphini) lang:pt since:2022-11-22 -filter:replies
 
-SearchTweetsParameters parameters = new(string.Join(" OR ", searchTerms))
+var filter = $"({string.Join(" OR ", searchTerms)})";// -filter:replies";
+
+SearchTweetsParameters parameters = new(filter)
 {
-    Since = dateSince,
+    Since = DateTime.Now,
     SearchType = Tweetinvi.Models.SearchResultType.Recent,
-    Filters = Tweetinvi.Parameters.Enum.TweetSearchFilters.Safe,
-    Locale= "br",   
+    Lang = Tweetinvi.Models.LanguageFilter.Portuguese,    
 };
-
 var searchResponse = await userClient.Search.SearchTweetsAsync(parameters);
 
-var tweets = searchResponse.ToList();
+var resultTweets = searchResponse.Where(tweet => !tweet.IsRetweet);
 
-tweets.First().PublishRetweetAsync().Wait();
+var tweets = resultTweets.OrderByDescending(t => t.CreatedAt).ToList();
+
+foreach (var tw in tweets)
+{
+    try
+    {
+        var allRetweets = await tw.GetRetweetsAsync();
+
+        if (!allRetweets.Any(x => x.CreatedBy.Id == user.Id)) // probably not necessary already treated in the query inside the parameters
+            await userClient.Tweets.PublishRetweetAsync(tw);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"error in Tweet {tw.Id} \n {ex.Message}");
+    }
+}
+
+
