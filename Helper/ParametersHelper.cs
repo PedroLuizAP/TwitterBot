612028109019d3f47c3998a7e0197a8a5de39fd0
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-29 17:00:33 -0300

    created ParametersHelpers

---
+++ Helper/ParametersHelper.cs
@@ -0,0 +1,21 @@
+﻿using Tweetinvi.Parameters;
+
+namespace TwitterBot.Helper
+{
+    internal class ParametersHelper
+    {
+        internal string CreateQuery(string[] terms)
+        {
+            return $"({string.Join(" OR ", terms)}) -filter:replies";
+        }
+
+        internal SearchTweetsParameters CreateParameters(string query)
+        {
+            return new(query)
+            {
+                Since = DateTime.Now,
+                SearchType = Tweetinvi.Models.SearchResultType.Recent,
+            };
+        }
+    }
+}
