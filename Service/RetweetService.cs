b0526c1d97c2d849d7be77688cec50aca6ec44cb
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-29 17:00:55 -0300

    create RetweetService

---
+++ Service/RetweetService.cs
@@ -0,0 +1,25 @@
+﻿using Tweetinvi;
+using Tweetinvi.Models;
+
+namespace TwitterBot.Service
+{
+    internal class RetweetService
+    {
+        protected TwitterClient _client { get; set; }
+
+        public RetweetService(TwitterClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<ITweet[]> GetAllRetweetUsers(ITweet tweet)
+        {
+            return await tweet.GetRetweetsAsync();
+        }
+
+        public bool RetweetedByMe(ITweet[] tweets, long userId)
+        {
+            return tweets.Any(x => x.CreatedBy.Id == userId);
+        }
+    }
+}
