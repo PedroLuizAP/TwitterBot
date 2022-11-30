74160f6f25125c274849be2b4e5c2849f3376192
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-30 11:01:58 -0300

    fix TweetService

--- Service/TweetService.cs
+++ Service/TweetService.cs
@@ -1,34 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Tweetinvi;
-using Tweetinvi.Models;
+﻿using Tweetinvi.Models;
+using Tweetinvi.Parameters;
 using TwitterBot.Helpers;
 
 namespace TwitterBot.Service
 {
     internal class TweetService
     {
+        private long _maxId { get; set; } = 0;
         protected ClientService _client { get; set; }
-        public TweetService(ClientService client)
-        {
-            _client = client;
-        }
+        public TweetService(ClientService client) => _client = client;
+        private void UseMaxId(SearchTweetsParameters parameters) => parameters.SinceId = _maxId;
+        private void SetMaxId(ITweet[] tweets) => _maxId = tweets.Max(tweet => tweet.Id);
 
-        internal async Task<List<ITweet>> FindByQuery(string query)
-        {
-            var tweets = await _client.Client.Search.SearchTweetsAsync(query);
-
-            return tweets.AsEnumerable().ToList();
-        }
-
-        internal async Task<List<ITweet>> FindByParameters(string query)
+        internal async Task<ITweet[]> FindByQuery(string query, bool updateMaxId = true)
         {
             var tweets = await _client.Client.Search.SearchTweetsAsync(query);
 
-            return tweets.AsEnumerable().ToList();
+            if (updateMaxId && tweets.Length > 0) SetMaxId(tweets);
+
+            return tweets;
         }
+
+        internal async Task<ITweet[]> FindByParameters(SearchTweetsParameters parameters, bool updateMaxId = true)
+        {
+            UseMaxId(parameters); 
+
+            var tweets = await _client.Client.Search.SearchTweetsAsync(parameters);
+
+            if (updateMaxId && tweets.Length > 0) SetMaxId(tweets);
+
+            return tweets;
+        }   
     }
 }
