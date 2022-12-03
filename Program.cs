dc0b5c7007b70124cc32abeb76d0d9b779284fbb
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-12-03 00:02:52 -0300

    refactor program.css

--- Program.cs
+++ Program.cs
@@ -1,58 +1,50 @@
-﻿using Tweetinvi;
-using Tweetinvi.Parameters;
+﻿using Tweetinvi.Parameters;
+using TwitterBot.Helper;
+using TwitterBot.Helpers;
+using TwitterBot.Service;
 
-long maxId = 0;
-
-var userClient = new TwitterClient(args[0], args[1], args[2], args[3]);
-
-var user = await userClient.Users.GetAuthenticatedUserAsync();
+ClientService clientService = new(args[0], args[1], args[2], args[3]);
 
 string[] searchTerms = new[]
-   {
-        "sseraphini",
-        "#paneladev",
-        "#bolhadev"
-    };
+{
+    "sseraphini",
+    "#paneladev",
+    "#bolhadev"
+};
 
 var dateSince = DateTime.Now;
 
-var filter = $"({string.Join(" OR ", searchTerms)}) -filter:replies";
+var query = searchTerms.CreateQuery();
 
-SearchTweetsParameters parameters = new(filter)
-{
-    Since = DateTime.Now,
-    SearchType = Tweetinvi.Models.SearchResultType.Recent,
-};
+SearchTweetsParameters parameters = query.CreateParameters();
+
+TweetService tweetService = new(clientService);
+
+RetweetService retweetService = new(clientService);
+
+var userId = (await clientService.DetailUser()).Id;
 
 while (true)
 {
-    parameters.SinceId = maxId;
-
-    var searchResponse = await userClient.Search.SearchTweetsAsync(parameters);
+    var searchResponse = await tweetService.FindByParameters(parameters);
 
     var resultTweets = searchResponse.Where(tweet => !tweet.IsRetweet);
 
     var tweets = resultTweets.OrderBy(t => t.CreatedAt).ToList();
 
     if (tweets != null && tweets.Count > 0)
-    {
         foreach (var tw in tweets)
-        {
             try
             {
-                var allRetweets = await tw.GetRetweetsAsync();
+                var allRetweets = await retweetService.GetAllRetweet(tw);
 
-                if (!allRetweets.Any(x => x.CreatedBy.Id == user.Id))
-                    await userClient.Tweets.PublishRetweetAsync(tw);
+                if (!retweetService.RetweetedByMe(allRetweets, userId)) await retweetService.Retweet(tw);
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"error in Tweet {tw.Id} \n {ex.Message}");
             }
-        }
 
-        maxId = tweets.Max(tweet => tweet.Id);
-    }
 
     Thread.Sleep(10000);
 }
