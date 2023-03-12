221fc5c21fc6fc22ffe238f5c1ad7cb55c63437d
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-03-11 21:30:18 -0300

    CreateMentionsParameters

--- Helper/ParametersHelper.cs
+++ Helper/ParametersHelper.cs
@@ -6,5 +6,6 @@ namespace TwitterBot.Helper
     {
         internal static string CreateQuery(this string[] terms, bool withReplies = true) => $"({string.Join(" OR ", terms)})" + (withReplies ? "-filter:replies" : string.Empty);
         internal static SearchTweetsParameters CreateParameters(this string query) => new(query) { Since = DateTime.Now, SearchType = Tweetinvi.Models.SearchResultType.Recent, };
+        internal static GetMentionsTimelineParameters CreateMentionsParameters() => new() { };
     }
 }
