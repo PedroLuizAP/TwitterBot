41b7d3474f9749fff8e8b5c66f6eb5c8565683d4
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-03-11 21:30:30 -0300

    create UserService

---
+++ Service/UserService.cs
@@ -0,0 +1,24 @@
+﻿using Tweetinvi.Models.DTO;
+using TwitterBot.Helpers;
+
+namespace TwitterBot.Service
+{
+    internal class UserService
+    {
+        protected ClientService _client { get; set; }
+        private IUserDTO? _user { get; set; }
+        public UserService(ClientService client) => _client = client;            
+        
+
+        public async Task GetUserInfo()
+        {
+            var user = await _client.Client.Users.GetAuthenticatedUserAsync();
+            
+            _user = user.UserDTO;
+        }
+
+        public long GetUserId() =>  _user!.Id;
+        public string GetUserScreenName() =>  _user!.ScreenName;
+        
+    }
+}
