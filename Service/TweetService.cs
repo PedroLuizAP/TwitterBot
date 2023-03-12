6838e699119cb29f60fabf76faf9b8d7d23eaf1c
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-03-11 21:30:50 -0300

    create FindMentions

--- Service/TweetService.cs
+++ Service/TweetService.cs
@@ -8,10 +8,13 @@ namespace TwitterBot.Service
     internal class TweetService
     {
         private long _maxId { get; set; } = 0;
+        private long _maxMentionId { get; set; } = 0;
         protected ClientService _client { get; set; }
         public TweetService(ClientService client) => _client = client;
         private void UseMaxId(SearchTweetsParameters parameters) => parameters.SinceId = _maxId;
+        private void UseMaxId(GetMentionsTimelineParameters parameters) => parameters.SinceId = _maxMentionId;
         private void SetMaxId(ITweet[] tweets) => _maxId = tweets.Max(tweet => tweet.Id);
+        private void SetMaxMentionId(ITweet[] tweets) => _maxMentionId = tweets.Max(tweet => tweet.Id);
 
         internal async Task<ITweet[]> FindByQuery(string query, bool updateMaxId = true)
         {
@@ -22,7 +25,7 @@ namespace TwitterBot.Service
             return tweets;
         }
 
-        internal async Task<ITweet[]> FindByParameters(SearchTweetsParameters parameters, bool updateMaxId = true)
+        internal async Task<ITweet[]?> FindByParameters(SearchTweetsParameters parameters, bool updateMaxId = true)
         {
             try
             {
@@ -34,10 +37,28 @@ namespace TwitterBot.Service
 
                 return tweets;
             }
-            catch(TwitterTimeoutException ex)
+            catch (TwitterTimeoutException)
             {
                 return null;
             }
-        }   
+        }
+
+        internal async Task<ITweet[]?> FindMentions(GetMentionsTimelineParameters parameters, bool updateMaxId = true)
+        {
+            try
+            {
+                UseMaxId(parameters);
+
+                var tweets = _maxMentionId > 0 ? await _client.Client.Timelines.GetMentionsTimelineAsync(parameters) : await _client.Client.Timelines.GetMentionsTimelineAsync();
+
+                if (updateMaxId && tweets.Length > 0) SetMaxMentionId(tweets);
+
+                return tweets;
+            }
+            catch (TwitterTimeoutException)
+            {
+                return null;
+            }
+        }
     }
 }
