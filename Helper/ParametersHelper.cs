ffa6970fab62e5f9509b64cce3c4187c6dfe4a8d
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-03-13 01:10:56 -0300

    adjust ParametersHelpers

--- Helper/ParametersHelper.cs
+++ Helper/ParametersHelper.cs
@@ -1,11 +1,12 @@
-﻿using Tweetinvi.Parameters;
+﻿using Tweetinvi.Models;
+using Tweetinvi.Parameters;
 
 namespace TwitterBot.Helper
 {
     internal static class ParametersHelper
     {
         internal static string CreateQuery(this string[] terms, bool withReplies = true) => $"({string.Join(" OR ", terms)})" + (withReplies ? "-filter:replies" : string.Empty);
-        internal static SearchTweetsParameters CreateParameters(this string query) => new(query) { Since = DateTime.Now, SearchType = Tweetinvi.Models.SearchResultType.Recent, };
+        internal static SearchTweetsParameters CreateParameters(this string query) => new(query) { Since = DateTime.Now, SearchType = SearchResultType.Recent };
         internal static GetMentionsTimelineParameters CreateMentionsParameters() => new() { };
     }
 }
