26b5672ff3f537038cc897a251be69f96c58fb0b
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-03-15 18:41:33 -0300

    create TweetsHelper

---
+++ Helper/TweetsHelper.cs
@@ -0,0 +1,32 @@
+﻿using Tweetinvi.Models;
+
+namespace TwitterBot.Helper
+{
+    public static class TweetsHelper
+    {
+        public static List<ITweet> FilterTweets(this ITweet[]? tweetsFilter, bool isMentions = false, string userScreenName = "")
+        {
+            List<ITweet> tweets = new List<ITweet>();
+
+            if (tweetsFilter?.Length > 0)
+            {
+                foreach (var tweet in tweetsFilter)
+                {
+                    if (isMentions && tweet.InReplyToStatusId != null && tweet.Text.Contains($"@{userScreenName}"))
+                    {
+                        tweet.FavoriteAsync().Wait();
+
+                        continue;
+                    }
+
+                    if (tweet.IsRetweet || tweet.InReplyToStatusId != null) continue;
+
+                    tweets.Add(tweet);
+                };
+            }
+
+            return tweets.OrderBy(t => t.CreatedAt).ToList();
+        }
+    }
+}
+
